1035a628001b907a58e3c6db45cfe424436e4923
Ankur Singh <chauhansinghcnkur1@gmail.com>
2017-10-20 17:30:54 +0530

    dfgh

---
+++ A.java
@@ -0,0 +1,29 @@
+class A{
+	public static void m\u0061in(String a1[]) throws InterruptedException {
+	System.out.println( "\\u" + Integer.toHexString('a' | 0x10000).substring(1) );
+	int i=10,j=20;
+	System.out.println(j--);
+	System.out.println(--i);
+	System.out.println(j++);
+	System.out.println(++i);
+	
+
+   
+      System.out.println("She said \"Hello!\" to me.");
+      for (int I = 0; I < 3; I++) {
+            System.out.print("Progress: " + I + " %\r");
+            Thread.sleep(1000);
+        }
+		
+		
+		System.out.println("Backspace      : " + "ABCDE\bFGHIJ");
+      System.out.println("Formfeed       : " + "ABCDE\fFGHIJ");
+      System.out.println("Linefeed       : " + "ABCDE\nFGHIJ");
+      System.out.println("Single Quote   : " + "ABCDE\'FGHIJ");
+      System.out.println("Double Quote   : " + "ABCDE\"FGHIJ");
+      System.out.println("Backslash      : " + "ABCDE\\FGHIJ");
+      System.out.println("Horizontal Tab : " + "ABCDE\tFGHIJ");
+      System.out.println("Carriage Return: " + "ABCDE\rFGHIJ");
+	};
+	
+}
